ef48b050263be7ef224b5f9fad3b372d9778f5cd
Valerii <Ihnatiev@users.noreply.github.com>
2019-08-21 23:26:39 +0300

    v3.0.6

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -27,63 +27,45 @@ exports.signup = (req, res, next) => {
 
 exports.login = (req, res, next) => {
   var email = req.body.email;
-  var password = req.body.password;
-  var dec_password = atob(password);
-  var encrypted_password = cryptr.encrypt(dec_password);
 
   sql.query("SELECT email, password FROM\
-  `user` WHERE email = '"+ email + "'",
+  User WHERE email = '"+ email + "'",
     function (err, results) {
-      if (results != "") {
+
+      if (results == "") {
+        res.status(500).json({
+          message: "Not a user"
+        });
+      } else if (results != "") {
+
         var data = JSON.stringify(results);
+        var secret = 'secret_this_should_be_longer';
         var jwtId = Math.random().toString(36).substring(7);
         var payload = {
           jwtid: jwtId,
-          audience: 'TEST',
           data: data
         };
-        jwt.sign(payload, 'secret_this_should_be_longer', { 
-          algorithm: 'HS256', 
-          expiresIn: '1h' 
-        },
-          function (err, token) {
-            if (err) {
-              res.json({
-                "results":
-                {
-                  "status": false,
-                  "msg": 'Error occurred while generating token'
-                }
-              });
-            } else {
-              if (token != false) {
-                res.header();
-                res.json({
-                  "results":
-                  {
-                    "status": true,
-                    "token": token,
-                    "user": results[0]
-                  }
-                });
-                res.end();
-              }
-              else {
-                res.json({
-                  "results":
-                    { "status": false, "msg": 'Could not create token' },
-                });
-                res.end();
-              }
-            }
-          });
-      }
-      else if (results == "") {
-        res.json({
-          "results":
-            { "status": false, "msg": 'Not found User!' }
+
+        jwt.sign(payload, secret, {
+          algorithm: 'HS256',
+          expiresIn: '1h'
+        }, function (err, token) {
+
+          if (err) {
+            return false;
+
+          } else if (token != false) {
+            res.status(200).json({
+              "results":
+                { status: "true" },
+              token: token
+            });
+            res.end();
+          } else {
+            res.send("Could not create token");
+            res.end();
+          }
         });
-        res.end();
       }
     });
 };
--- src/js/controllers/LoginCtrl.js
+++ src/js/controllers/LoginCtrl.js
@@ -1,27 +1,28 @@
 'use strict';
 var app = angular.module('EmpApp')
-  .controller('LoginCtrl', ['$scope', '$http', '$log', function ($scope, $http, $log) {
-    $scope.word = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
-
-    $scope.onLogin = () => {
-      $http({
-        method: 'POST',
-        url: 'http://localhost:3002/api/user/login',
-        data: {
-          'email': $scope.userEmail,
-          'password': $scope.userPassword
-        },
-        headers: { 'Content-Type': 'application/JSON' }
-      }).then(
-        response => {
-          document.forms["login"].reset();
-          $log.info(response);
-        })
-        .catch(
-          error => {
-            alert(error);
+  .controller('LoginCtrl', ['$scope', '$http', '$log',
+    function ($scope, $http, $log) {
+      $scope.word = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
+      
+      $scope.onLogin = () => {
+        $http({
+          method: 'POST',
+          url: 'http://localhost:3002/api/user/login',
+          data: {
+            'email': $scope.email,
+            'password': $scope.password
+          },
+          headers: { 'Content-Type': 'application/JSON' }
+        }).then(
+          response => {
             document.forms["login"].reset();
-            $log.info(error);
-          });
-    };
-  }]);
+            $log.info(response);
+          })
+          .catch(
+            error => {
+              alert(error);
+              document.forms["login"].reset();
+              $log.info(error);
+            });
+      };
+    }]);
