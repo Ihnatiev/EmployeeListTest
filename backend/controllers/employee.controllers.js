c3c0d40b6c8eb9a4aa049987ca8b8448a1dcae59
Valerii <Ihnatiev@users.noreply.github.com>
2019-09-03 13:41:04 +0300

    Added pagination to employee.controllers.js

--- backend/controllers/employee.controllers.js
+++ backend/controllers/employee.controllers.js
@@ -1,20 +1,45 @@
 const Employee = require('../services/employee');
+const Promise = require('bluebird');
+const connection = require('../config/connection');
+
+var queryAsync = Promise.promisify(connection.query.bind(connection));
 
 exports.getAllEmployees = (req, res) => {
-  Employee.displayEmployees((err, employees) => {
-    if (err) {
-      res.status(500).json({
-        success: false,
-        message: 'Fetching employees failed!'
-      });
-    } else {
-      res.status(200).json({
-        success: true,
-        message: 'Employees fetched successfully!',
-        employees: employees
-      });
-    };
-  });
+  var numRows;
+  var numPages;
+  var numPerPage = parseInt(req.query.npp, 10) || 5;
+  var page = parseInt(req.query.page, 10) || 0;
+  var skip = page * numPerPage;
+  var end_limit = numPerPage;
+  var limit = skip + ',' + end_limit;
+  queryAsync('SELECT count(*) as numRows FROM Employee')
+    .then((results) => {
+      numRows = results[0].numRows;
+      numPages = Math.ceil(numRows / numPerPage);
+    })
+    .then(() => queryAsync("SELECT empID, empName, IF(empActive, 'Yes', 'No')\
+  empActive, dpName FROM Employee\
+  INNER JOIN Department ON empDepartment = dpID  LIMIT " + limit))
+    .then((results) => {
+      var responsePayload = {
+        results: results
+      };
+      if (page < numPages) {
+        responsePayload.pagination = {
+          previous: page > 0 ? page - 1 : undefined,
+          current: page,
+          perPage: numPerPage,
+          next: page < numPages - 1 ? page + 1 : undefined
+        }
+      }
+      else responsePayload.pagination = {
+        err: 'queried page ' + page + ' is >= to maximum page number ' + numPages
+      }
+      res.status(200).json(responsePayload);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: err });
+    });
 };
 
 exports.createAnEmployee = (req, res) => {
