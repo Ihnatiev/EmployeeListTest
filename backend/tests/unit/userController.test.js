8e4ffd47805475ed129192268b8a7ee58a63c153
Valerii <Ihnatiev@users.noreply.github.com>
2019-11-22 19:39:10 +0200

    Final update userController.test.js

--- backend/tests/unit/userController.test.js
+++ backend/tests/unit/userController.test.js
@@ -9,7 +9,6 @@ const mockRequest = (body) => ({
 const mockResponse = () => {
   const res = {};
   res.status = jest.fn().mockReturnValue(res);
-  res.sendStatus = jest.fn().mockReturnValue(res);
   res.json = jest.fn().mockReturnValue(res);
   return res;
 };
