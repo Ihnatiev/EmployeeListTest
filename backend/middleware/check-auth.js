4b4454c4662c49a03f5b4954c609f00d90f11936
Valerii <Ihnatiev@users.noreply.github.com>
2019-11-22 20:37:41 +0200

    Update check-auth.js

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+const secret = require('../config/secret');
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'this_secret_should_be_longer');
+    const decodedToken = jwt.verify(token, secret.jwtSecret);
     req.userData = {
       email: decodedToken.email,
       userId: decodedToken.userId
