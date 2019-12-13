b118fbf4d11faa2cef0beb7783e4270e1c6ef090
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-13 23:58:30 +0200

    Update userController.test.js

--- backend/tests/unit tests/controller tests/userController.test.js
+++ backend/tests/unit tests/controller tests/userController.test.js
@@ -15,158 +15,65 @@ const mockResponse = () => {
 
 describe('UserController', () => {
   describe('SignUp', () => {
-    describe('test body request', () => {
-      test('should return status 400 if smth is missing from body', () => {
-        var req = mockRequest(
-          { email: 'boss@mail.com', password: 'boss' }
-        );
-        const res = mockResponse();
-
-        controller.signup(req, res);
-
-        expect(res.status).toHaveBeenCalledWith(400);
-        expect(res.json).toHaveBeenCalledWith({
-          success: false,
-          message: 'Please pass name, email and password.'
-        });
-      });
-      test('should return status 400 if email is missing from body', () => {
-        var req = mockRequest(
-          { name: 'Bob', password: 'boss' }
-        );
-        const res = mockResponse();
-
-        controller.signup(req, res);
-
-        expect(res.status).toHaveBeenCalledWith(400);
-        expect(res.json).toHaveBeenCalledWith({
-          success: false,
-          message: 'Please pass name, email and password.'
-        });
-      });
-      test('should return status 400 if password is missing from body', () => {
-        var req = mockRequest(
-          { name: 'Bob', email: 'boss@mail.com' }
-        );
-        const res = mockResponse();
-
-        controller.signup(req, res);
-
-        expect(res.status).toHaveBeenCalledWith(400);
-        expect(res.json).toHaveBeenCalledWith({
-          success: false,
-          message: 'Please pass name, email and password.'
-        });
-      });
-    });
-    describe('test user creation', () => {
-      test('should return status 201 if user created successfully', async () => {
-        const req = mockRequest(
-          {
-            name: 'Peter',
-            email: 'smith@mail.ru',
-            password: 'jsmith'
-          }
-        );
-        const res = mockResponse();
-        const expectedUser = {
-          name: 'Peter',
-          email: 'smith@mail.ru',
-          password: expect.anything()
-        };
-
-        const spySave = jest.spyOn(service, "save").mockImplementation((err, result) => {
-          return result(null, 2);
-        });
-
-        await controller.signup(req, res);
-
-        expect(spySave).toHaveBeenCalledTimes(1);
-        expect(spySave).toBeCalledWith(expectedUser, expect.anything());
-        expect(res.status).toHaveBeenCalledWith(201);
-        expect(res.json).toHaveBeenCalledWith({
-          success: true,
-          message: 'User created!',
-          userId: 2
-        });
-        spySave.mockClear();
-      });
-
-      test('should return status 500 if error from database', async () => {
-        const req = mockRequest(
-          {
-            name: 'Peter',
-            email: 'smith@mail.ru',
-            password: 'jsmith'
-          }
-        );
-        const res = mockResponse();
-
-        const spySave = jest.spyOn(service, "save").mockImplementation((err, result) => {
-          return result(err, null);
-        });
-
-        await controller.signup(req, res);
-
-        expect(spySave).toHaveBeenCalledTimes(1);
-        expect(res.status).toHaveBeenCalledWith(500);
-        expect(res.json).toHaveBeenCalledWith({
-          success: false,
-          message: 'Database error'
-        });
-        spySave.mockClear();
-      });
-
-      test('should return status 500 if error from server', async () => {
-        const req = mockRequest(
-          {
-            name: 'Peter',
-            email: 'smith@mail.ru',
-            password: 'jsmith'
-          }
-        );
-        const res = mockResponse();
-
-        const spySave = jest.spyOn(service, "save").mockImplementation((result) => {
-          return result();
-        });
-
-        await controller.signup(req, res);
-
-        expect(spySave).toHaveBeenCalledTimes(1);
-        expect(res.status).toHaveBeenCalledWith(500);
-        expect(res.json).toHaveBeenCalledWith({
-          message: 'Invalid authentication credentials!'
-        });
-        spySave.mockClear();
-      });
-    });
-  });
-
-  describe('LogIn', () => {
-    test('should return status 500 if authentication failed', () => {
+    test('should return status 201 if user created successfully', () => {
       const req = mockRequest(
         {
-          email: 'smit@mail.ru',
+          name: 'Peter',
+          email: 'smith@mail.ru',
+          password: 'jsmith'
+        }
+      );
+      const res = mockResponse();
+      const newUserData = {
+        name: 'Peter',
+        email: 'smith@mail.ru',
+        password: 'jsmith'
+      };
+
+      const spySave = jest.spyOn(service, "save").mockImplementation((err, result) => {
+        return result(expect.anything(), null)
+      });
+
+      controller.signup(req, res);
+
+      expect(spySave).toHaveBeenCalledTimes(1);
+      expect(spySave).toBeCalledWith(newUserData, expect.anything());
+      expect(res.status).toHaveBeenCalledWith(201);
+      expect(res.json).toHaveBeenCalledWith({
+        success: true,
+        message: 'User created!',
+        userId: expect.anything()
+      });
+      spySave.mockClear();
+    });
+
+    test('should return status 500 if error from database', () => {
+      const req = mockRequest(
+        {
+          name: 'Peter',
+          email: 'smith@mail.ru',
           password: 'jsmith'
         }
       );
       const res = mockResponse();
 
-      const spyFind = jest.spyOn(service, "find").mockImplementation((error, result) => {
-        return result(error, []);
+      const spySave = jest.spyOn(service, "save").mockImplementation((err, result) => {
+        return result(null, err);
       });
 
-      controller.login(req, res);
+      controller.signup(req, res);
 
-      expect(spyFind).toHaveBeenCalledTimes(1);
+      expect(spySave).toHaveBeenCalledTimes(1);
       expect(res.status).toHaveBeenCalledWith(500);
       expect(res.json).toHaveBeenCalledWith({
         success: false,
-        message: "Authentication failed."
+        message: 'This email already exists.'
       });
-      spyFind.mockClear();
+      spySave.mockClear();
     });
+  });
+
+  describe('LogIn', () => {
     test('should return status 401 if email and password does not match', async () => {
       const req = mockRequest(
         {
@@ -176,10 +83,10 @@ describe('UserController', () => {
       );
       const res = mockResponse();
 
-      const spyFind = jest.spyOn(service, "find").mockImplementation((error, results) => {
+      const spyFind = jest.spyOn(service, "findOne").mockImplementation((error, results) => {
         return results(null, [
           {
-            id: 1,
+            id: expect.anything(),
             name: "Peter",
             email: "smith@mail.ru",
             password: "$2b$10$Y/cjeKxQrb/QOSlIs41Cf.3HHtNiraeVePYo28po7x9WwuynTzBVm"
@@ -197,6 +104,29 @@ describe('UserController', () => {
       })
       spyFind.mockClear();
     });
+    test('should return status 500 if authentication failed', async () => {
+      const req = mockRequest(
+        {
+          email: 'smit@mail.ru',
+          password: 'jsmith'
+        }
+      );
+      const res = mockResponse();
+
+      const spyFind = jest.spyOn(service, "findOne").mockImplementation((error, result) => {
+        return result(error, []);
+      });
+
+      await controller.login(req, res);
+
+      expect(spyFind).toHaveBeenCalledTimes(1);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        status: 'error',
+        message: "Authentication failed."
+      });
+      spyFind.mockClear();
+    });
     test('should return status 200 if user authenticated', async () => {
       const req = mockRequest(
         {
@@ -205,10 +135,10 @@ describe('UserController', () => {
         }
       );
 
-      const spyFind = jest.spyOn(service, "find").mockImplementation((error, results) => {
+      const spyFind = jest.spyOn(service, "findOne").mockImplementation((error, results) => {
         return results(null, [
           {
-            id: 1,
+            id: expect.anything(),
             name: "Peter",
             email: "smith@mail.ru",
             password: "$2b$10$Y/cjeKxQrb/QOSlIs41Cf.3HHtNiraeVePYo28po7x9WwuynTzBVm"
@@ -233,4 +163,3 @@ describe('UserController', () => {
     });
   });
 });
-
