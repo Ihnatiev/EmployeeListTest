7d24eae9869d676eac2248c2e89b6012f6e3f6e0
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-13 23:59:02 +0200

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const swagger = require('../swagger/user/validator');
+const {checkUserCreate, checkUserLogin} = require('../middleware/checkUser');
+const userController = require('../controllers/userControllers');
 
-const UserController = require('../controllers/userControllers');
-
-router.post('/signup', UserController.signup);
-router.post('/login', UserController.login);
+router.post('/signup', swagger.validateBody('new-user'), checkUserCreate, userController.signup);
+router.post('/login', swagger.validateBody('user-login'), checkUserLogin, userController.login);
 
 module.exports = router;
 
