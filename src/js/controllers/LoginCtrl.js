642d72326aca6377896a6a54381a9b0769088abd
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-19 19:44:00 +0200

    Update LoginCtrl.js

--- src/js/controllers/LoginCtrl.js
+++ src/js/controllers/LoginCtrl.js
@@ -1,16 +1,43 @@
-'use strict'
+(function () {
+  'use strict'
 
-angular.module('app')
-.controller('LoginCtrl', ['authService', '$scope', '$location', function (authService, $scope, $location) {
-  $scope.signin = () => {
-    $scope.credentials = {
-      email: $scope.email,
-      password: $scope.password
+  angular
+    .module('app')
+    .controller('LoginCtrl', ['$state', 'authService', LoginCtrl]);
+  function LoginCtrl($state, authService) {
+    var vm = this;
+    vm.LoginError = false
+    vm.LoginErrorMessage = null;
+
+    vm.login = login;
+
+    function login() {
+      vm.LoginError = false
+      vm.LoginErrorMessage = null;
+
+      if (!vm.email || !vm.password) {
+        vm.LoginError = true;
+        vm.LoginErrorMessage = 'Email and password required!';
+        return;
+      }
+      
+
+      authService.login(vm.email, vm.password)
+        .then(handleSuccessfulLogin)
+        .catch(handleFailedLogin);
     }
-    authService.loginUser($scope.credentials);
-    $location.path('/employees');
-  }
-  
-}]);
 
+    function handleSuccessfulLogin() {
+      $state.go('index');
+    }
+
+    function handleFailedLogin(response) {
+      if (response && response.data) {
+        vm.LoginErrorMessage = response.data.message;
+        vm.LoginError = true;
+      }
+    }
+
+  };
+})();
 
