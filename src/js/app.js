1c8552c32d4e5e20fa6962cde1e0aa763f69565b
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-19 19:43:39 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,34 @@
-var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'ngStorage', 'ngCookies'])
-  .config(config);
-// .run(run);
-function config($stateProvider, $urlRouterProvider) {
-  // default route
-  
+var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'ngStorage', 'ngCookies']);
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  '$httpProvider',
+  '$locationProvider',
+  authConfig
+])
+
+function authConfig(
+  $stateProvider,
+  $urlRouterProvider,
+  $httpProvider,
+  $locationProvider) {
+
   $stateProvider
-    .state('signIn', {
-      url: '/user/login',
-      templateUrl: 'auth/login/loginUser.html',
-      controller: 'LoginCtrl'
-    })
-    .state('createUser', {
-      url: '/user/signup',
-      templateUrl: 'auth/signup/signupUser.html',
-      controller: 'SignupCtrl',
-      authenticate: true
-    })
-    .state('allEmployees', {
+    .state('index', {
       url: '/employees',
       templateUrl: 'views/empList.html',
       controller: 'EmployeeCtrl'
+    })
+    .state('login', {
+      url: '/user/login',
+      templateUrl: 'auth/login/loginUser.html',
+      controller: 'LoginCtrl as lc'
+    })
+    .state('signup', {
+      url: '/user/signup',
+      templateUrl: 'auth/signup/signupUser.html',
+      controller: 'SignupCtrl as sc',
+      authenticate: true
     })
     .state('view.id', {
       url: '/employees/:employeeId',
@@ -33,12 +42,32 @@ function config($stateProvider, $urlRouterProvider) {
       controller: 'EmployeeCtrl',
       authenticate: true
     });
-    $urlRouterProvider.otherwise("/employees");
+
+  $urlRouterProvider.otherwise("/employees");
 };
 
+app.run([
+  '$rootScope',
+  '$state',
+  'authService',
+  authRun
+]);
+
+function authRun($rootScope, $state, authService) {
+  $rootScope.$on('$stateChangeStart', function (event, toState) {
+    if (toState.data && toState.data.accessLevel) {
+      var user = authService.getUserData();
+      if (!(toState.data.accessLevel & user.role)) {
+        event.preventDefault();
+        $state.go('index');
+        return;
+      }
+    }
+  });
+}
 function run($rootScope, $http, $location, $localStorage) {
   if ($localStorage.currentUser) {
-    $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.currentUser.token;
+    $http.defaults.headers.common.Authorization = 'Basic ' + $localStorage.currentUser.token;
   }
 
   $rootScope.$on('$locationChangeStart', function (event, next, current) {
