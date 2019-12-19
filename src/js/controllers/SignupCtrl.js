bd82641771ce7423c0a6cfdf31158abaa6fd5569
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-19 19:43:55 +0200

    Update SignupCtrl.js

--- src/js/controllers/SignupCtrl.js
+++ src/js/controllers/SignupCtrl.js
@@ -1,26 +1,47 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('app')
-  .controller('SignupCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+  angular.module('app')
+    .controller('SignupCtrl', ['$scope', 'authService', SignupCtrl]);
+  function SignupCtrl($scope, authService) {
+    var vm = this;
+
+    vm.signupSuccess = false;
+    vm.signupError = false
+    vm.signupErrorMessage = null;
+
     $scope.word = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
-    
-    $scope.onSignup = () => {
-      $http({
-        method: 'POST',
-        url: 'https://localhost:6969/api/auth/signup',
-        data: {
-          name: $scope.name,
-          email: $scope.email,
-          password: $scope.password
-        },
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(function (result) {
-        alert(result.data.message);
-        $location.path('/user/login');
-      }, function (error) {
-        alert(error.data.message);
-      });
-    };
-  }]);  
+
+    vm.signup = signup;
+
+    function signup() {
+      vm.signupSuccess = false;
+      vm.signupError = false
+      vm.signupErrorMessage = null;
+
+      if (!vm.name ||!vm.email || !vm.password) {
+        vm.signupError = true;
+        vm.signupErrorMessage = 'Missing required fields';
+        return;
+      }
+
+      authService.signup(vm.name, vm.email, vm.password)
+        .then(handleSuccessfulSignup)
+        .catch(handleFailedSignup);
+    }
+
+    function handleSuccessfulSignup(response) {
+      vm.signupSuccess = true;
+    }
+
+    function handleFailedSignup(response) {
+      vm.signupSuccess = false;
+
+      if (response && response.data) {
+        vm.signupErrorMessage = response.data.message;
+        vm.signupError = true;
+      }
+    }
+  }
+
+})();
