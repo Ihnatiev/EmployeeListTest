88b415055b1d33ae57ad0dc125ca64e925a504e3
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-22 16:59:04 +0200

    Update employeeControllers.js

--- backend/controllers/employeeControllers.js
+++ backend/controllers/employeeControllers.js
@@ -5,18 +5,21 @@ module.exports = {
     try {
       EmployeeService.find(req.params.employeeId,
         (err, employee) => {
-          (employee == 0 || '') ?
-            res.status(404).json({
+          if (employee == 0) {
+            return res.status(404).json({
               success: false,
               message: 'Employee not found!'
-            }) :
-            res.status(200).json({
+            });
+          };
+          if (employee != 0) {
+            return res.status(200).json({
               success: true,
               employee: employee
             });
+          };
         });
     } catch (err) {
-      res.status(500).json({
+      return res.status(500).json({
         success: false,
         message: 'Server error'
       });
@@ -30,19 +33,20 @@ module.exports = {
       const count = await EmployeeService.getCount();
       const results = await EmployeeService.findAll(numPerPage, page);
       const totalEmployee = count[0].totalCount;
-      if (totalEmployee === 0) {
-        res.status(404).json({
+      if (totalEmployee == 0) {
+        return res.status(404).json({
           success: false,
           message: 'Employee not found'
         });
-      } else if (count && results) {
-        res.status(200).json({
+      };
+      if (count && results) {
+        return res.status(200).json({
           employees: results,
           maxEmployees: totalEmployee
         });
       };
     } catch {
-      res.status(500).json({
+      return res.status(500).json({
         success: false,
         message: 'Server error'
       });
@@ -52,67 +56,83 @@ module.exports = {
   createEmployee: (req, res) => {
     const emp = { empName, empActive, empDepartment } = req.body;
     const creator = req.userData.userId;
-    EmployeeService.create(emp, creator,
-      (err, result) => {
-        (err) ?
-          res.status(500).json({
-            success: false,
-            message: 'Adding employee failed!'
-          }) :
-          res.status(201).json({
-            success: true,
-            message: 'Employee added successfully!',
-            employeeId: result
-          });
+    try {
+      EmployeeService.create(emp, creator,
+        (err, result) => {
+          if (err) {
+            return res.status(500).json({
+              success: false,
+              message: 'Adding employee failed!'
+            });
+          };
+          if (result) {
+            return res.status(201).json({
+              success: true,
+              message: 'Employee added successfully!',
+              employeeId: result
+            });
+          };
+        });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Adding an employee failed!"
       });
+    };
   },
 
   updateEmployeeById: (req, res) => {
     const employeeId = req.params.employeeId;
     const creator = req.userData.userId;
     const emp = { empName, empActive, empDepartment } = req.body;
-    EmployeeService.update(employeeId, creator, emp,
-      (err, result) => {
-        try {
-          (result.affectedRows > 0) ?
-            res.status(200).json({
+    try {
+      EmployeeService.update(employeeId, creator, emp,
+        (err, result) => {
+          if (result.affectedRows > 0) {
+            return res.status(200).json({
               success: true,
               message: 'Update successfully!'
-            }) :
-            res.status(401).json({
+            });
+          };
+          if (result.affectedRows == 0) {
+            return res.status(401).json({
               success: false,
               message: 'You are not authorized!'
             });
-        } catch (error) {
-          res.status(500).json({
-            success: false,
-            message: "Updating an employee failed!"
-          });
-        };
+          };
+        });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Updating an employee failed!"
       });
+    };
   },
 
   deleteEmployeeById: (req, res) => {
     const employeeId = req.params.employeeId;
     const creator = req.userData.userId;
-    EmployeeService.delete(employeeId, creator,
-      (err, result) => {
-        try {
-          (result.affectedRows > 0) ?
-            res.status(200).json({
+    try {
+      EmployeeService.delete(employeeId, creator,
+        (err, result) => {
+          if (result.affectedRows > 0) {
+            return res.status(200).json({
               success: true,
               message: 'Deletion successful!'
-            }) :
-            res.status(401).json({
+            });
+          };
+          if (result.affectedRows == 0) {
+            return res.status(401).json({
               success: false,
               message: 'You are not authorized!'
             });
-        } catch (error) {
-          res.status(500).json({
-            success: false,
-            message: "Deleting an employee failed!"
-          });
-        };
+          };
+        });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Deleting an employee failed!"
       });
+    };
   }
-}
+};
