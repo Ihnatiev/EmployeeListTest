fbc44b2533e39211cd176e7125449689afe0ec73
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-22 14:46:23 +0200

    Create cors.js

---
+++ backend/helpers/cors.js
@@ -0,0 +1,14 @@
+module.exports.init = app =>
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+  });
+  
