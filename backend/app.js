77ab6b5c8f1a393ee72fb10963e819861c2a848e
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-22 14:46:11 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,17 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const employeeRoutes = require('./routes/employee.routes');
 const userRoutes = require('./routes/user.routes');
 const swaggerUi = require('swagger-ui-express');
 const swagDoc = require('./swagger/swagger.json');
 const { handleError } = require('./middleware/errorHandler');
-
-const app = express();
+require('./helpers/cors').init(app);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagDoc));
 app.use("/api/employees", employeeRoutes);
