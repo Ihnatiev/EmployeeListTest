ea947f80aec4af9d7b3f976781b3027e87f598ab
Valerii <Ihnatiev@users.noreply.github.com>
2019-12-22 16:07:10 +0200

    Update checkUser.js

--- backend/middleware/checkUser.js
+++ backend/middleware/checkUser.js
@@ -1,6 +1,6 @@
 const checkUserCreate = (req, res, next) => {
   if (!req.body.name || !req.body.email || !req.body.password) {
-    return resizeBy.status(400).send('Invalid data');
+    return res.status(400).send('Invalid data');
   } else {
     next();
   }
@@ -8,7 +8,7 @@ const checkUserCreate = (req, res, next) => {
 
 const checkUserLogin = (req, res, next) => {
   if (!req.body.email || !req.body.password) {
-    return resizeBy.status(400).send('Invalid data');
+    return res.status(400).send('Invalid data');
   } else {
     next();
   }
